9a83a5abfd59ac36179f092f85e8c686aebb3e85
TJ-Projects3 <tjprojects3@gmail.com>
2025-01-12 07:35:02 -0500

    Implemented middleware, improved endpoint, and began Postman testing

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,21 @@
 import express from 'express';
 import dotenv from 'dotenv'
 import { connectDB } from './config/db.js'
+import Product from './models/products.js'
 
 dotenv.config();
 
 const app = express();
 
-app.get("/", async (req, res) => {
-    const product = req.body; // user sends this data
+
+app.use(express.json()) // This allows us to accept JSON data in the req.body
+// Serves as the middleware before the client hits API and gets response from the server.
+
+app.post("/api/products", async (req, res) => {
+    const product = req.body; // User sends this data
 
     if (!product.name || !product.price || !product.image) {
-        return res.send(400).json( { success: false, message: "Please provide all required fields"})
+        return res.status(400).json( { success: false, message: "Please provide all required fields"})
     }
 
     const newProduct = new Product(product)
@@ -22,6 +27,8 @@ app.get("/", async (req, res) => {
         res.status(500).json({ success: false, message: "Server Error" })
     }
 });
+
+
 
 // Testing using Postman...
 
