adc0589d9b596f0b5767570d3f6e4685e640823b
TJ-Projects3 <tjprojects3@gmail.com>
2025-01-21 13:13:06 -0500

    Implemented React Hot Toast for CreatePage

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,12 +3,14 @@ import { Provider } from "@/components/ui/provider"
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import { Toaster } from 'react-hot-toast'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
      <BrowserRouter>
      <Provider>
       <App />
+      <Toaster position='bottom-center' />
     </Provider>
    </BrowserRouter>
    </StrictMode>,
--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -1,64 +1,66 @@
 import { useColorModeValue } from "@/components/ui/color-mode";
 import { useProductStore } from "@/store/product";
-import { Container, VStack, Box, Heading, Input, Button } from "@chakra-ui/react";
+import { Container, VStack, Box, Heading, Input, Button} from "@chakra-ui/react";
 import { useState } from "react";
+import toast from "react-hot-toast";
 
 const CreatePage = () => {
-    const [newProduct, setNewProduct] = useState ({
+    const [newProduct, setNewProduct] = useState({
         name: "",
         price: "",
         image: "",
     });
-    const { createProduct } = useProductStore()
-    const handleAddProduct = async() => {
-       const {success, message} = await createProduct(newProduct)
-       console.log("Success:", success)
-       console.log("Message:", message)
-    }
-  return (
-    <Container maxW={"container.sm"}>
-        <VStack
-            spacing={8}
-        >
-            <Heading as={"h1"} size={"2xl"} textAlign={"center"} mb={8}>
-                Create New Product
-            </Heading>
+    const { createProduct } = useProductStore();
 
-        <Box
-            w={"full"} bg={useColorModeValue("white, gray.800")}
-            p={6} rounded={"lg"} shadow={"md"}
-        >
+    const handleAddProduct = async () => {
+        const { success, message } = await createProduct(newProduct);
+        console.log("Success:", success);
+        console.log("Message:", message);
+        return success ? toast.success(message) : toast.error(message)
+    };
 
-            <VStack spacing={4}>
-                <Input
-                placeholder="Product Name"
-                name='name'
-                value={newProduct.name}
-                onChange={(e) => setNewProduct({...newProduct, name: e.target.value})}
-                />
-                <Input
-                placeholder="Product Price"
-                name='price'
-                type="number"
-                value={newProduct.price}
-                onChange={(e) => setNewProduct({...newProduct, price: e.target.value})}
-                />
-                <Input
-                type="Image URL" 
-                placeholder="Product Image"
-                name="image"
-                value={newProduct.image}
-                onChange={(e) => setNewProduct({...newProduct, image: e.target.value})}
-                />
-                <Button colorScheme={'dark'} w={'full'} onClick={handleAddProduct}>
-                    Add Product
-                </Button>
+    return (
+        <Container maxW={"container.sm"}>
+            <VStack spacing={8}>
+                <Heading as={"h1"} size={"2xl"} textAlign={"center"} mb={8}>
+                    Create New Product
+                </Heading>
+
+                <Box
+                    w={"full"}
+                    bg={useColorModeValue("white", "gray.800")}
+                    p={6}
+                    rounded={"lg"}
+                    shadow={"md"}
+                >
+                    <VStack spacing={4}>
+                        <Input
+                            placeholder="Product Name"
+                            name='name'
+                            value={newProduct.name}
+                            onChange={(e) => setNewProduct({ ...newProduct, name: e.target.value })}
+                        />
+                        <Input
+                            placeholder="Product Price"
+                            name='price'
+                            type="number"
+                            value={newProduct.price}
+                            onChange={(e) => setNewProduct({ ...newProduct, price: e.target.value })}
+                        />
+                        <Input
+                            placeholder="Product Image"
+                            name="image"
+                            value={newProduct.image}
+                            onChange={(e) => setNewProduct({ ...newProduct, image: e.target.value })}
+                        />
+                        <Button colorScheme={'blue'} w={'full'} onClick={handleAddProduct}>
+                            Add Product
+                        </Button>
+                    </VStack>
+                </Box>
             </VStack>
-        </Box>
-        </VStack>
-
-    </Container>
-  )
+        </Container>
+    );
 }
 
-export default CreatePage
+export default CreatePage;
